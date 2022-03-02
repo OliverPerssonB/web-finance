316cfa0809bd5aa6be84742224fd1b1144d116eb
oliver_persson_b <oliver.persson.b@gmail.com>
2022-03-02 09:54:19 +0100

    navigation working

--- finance/src/app/app-routing.module.ts
+++ finance/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartsComponent } from './charts/charts.component';
+import { PickStocksComponent } from './pick-stocks/pick-stocks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "overview", component: ChartsComponent },
+  { path: "charts", component: ChartsComponent },
+  { path: "pick-stocks", component: PickStocksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
