73f76902a083e02395e68bdd07acf2ff49a3ff19
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2022-03-03 14:49:35 +0100

    Add initial form

--- finance/src/app/app.module.ts
+++ finance/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,8 @@ import { PickStocksComponent } from './pick-stocks/pick-stocks.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
