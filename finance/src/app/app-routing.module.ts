e46d252107fb742459d6ef2c18dcd4e3a7a8bed6
oliver_persson_b <oliver.persson.b@gmail.com>
2022-03-04 10:00:37 +0100

    added welcome page

--- finance/src/app/app-routing.module.ts
+++ finance/src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { ChartsComponent } from './charts/charts.component';
 import { OverviewComponent } from './overview/overview.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PickStocksComponent } from './pick-stocks/pick-stocks.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 const routes: Routes = [
+  { path: "welcome-page", component: WelcomePageComponent },
   { path: "overview", component: OverviewComponent },
   { path: "charts", component: ChartsComponent },
   { path: "pick-stocks", component: PickStocksComponent },
+  { path: '', redirectTo: '/welcome-page', pathMatch: 'full' },
   { path: "**", component: PageNotFoundComponent },
 ];
 
