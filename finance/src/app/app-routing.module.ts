95aa11ce6b20dc88d836686b7f76969d777d592b
oliver_persson_b <oliver.persson.b@gmail.com>
2022-03-04 09:57:23 +0100

    added page not found

--- finance/src/app/app-routing.module.ts
+++ finance/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChartsComponent } from './charts/charts.component';
 import { OverviewComponent } from './overview/overview.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PickStocksComponent } from './pick-stocks/pick-stocks.component';
 
 const routes: Routes = [
   { path: "overview", component: OverviewComponent },
   { path: "charts", component: ChartsComponent },
   { path: "pick-stocks", component: PickStocksComponent },
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
