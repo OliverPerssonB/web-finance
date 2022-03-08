4f9220fa2f8c0f8b3e3d57ef3a6ec33d5f0598f4
Signe Berden <si3087be-s@lo-6.e.lth.lu.se>
2022-03-08 18:55:05 +0100

    Fixed welcome-page

--- finance/src/app/welcome-page/welcome-page.component.ts
+++ finance/src/app/welcome-page/welcome-page.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent implements OnInit {
-
+  page = {
+    title: 'Welcome to !Avanza',
+    subtitle: 'Get a grip on your stocks!',
+    content: 'Use the overview to scroll amongst indices and use the chart to see a graph over picked stock. ',
+    image: 'assets/backgroundd.jpg'
+  };
   constructor() { }
 
   ngOnInit(): void {
